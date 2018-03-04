6aa965a26a511a1531b19acbbdd12dd6883eea22
inigoserrano <inigo.serrano@gmail.com>
2018-03-04 07:48:38 +0100

    cambio

--- src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
+++ src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
@@ -3,7 +3,7 @@ package com.inigoserrano.openshift.aplicacion;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.DriverManager; 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
