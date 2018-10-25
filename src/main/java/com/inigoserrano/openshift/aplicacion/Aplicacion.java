745e02db54f1cc7ce7989e35449c1a0d8cede1d0
condestable2000 <condestable2000@users.noreply.github.com>
2018-10-25 10:06:51 +0200

    Update Aplicacion.java

--- src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
+++ src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
@@ -24,8 +24,8 @@ public class Aplicacion extends HttpServlet {
 
 	@Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("Salida GET");
-        response.getWriter().println("Salida GET version master");  
+        System.out.println("Salida GET modificada");
+        response.getWriter().println("Salida GET version master 2");  
         response.getWriter().flush();
     }
 
