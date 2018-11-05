9da45b4ae12eb0f8d47a1b4dcb99ddd7198b883e
twinsara <twinsara@gmail.com>
2018-11-05 13:24:44 +0100

    Update Aplicacion.java

--- src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
+++ src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
@@ -25,7 +25,7 @@ public class Aplicacion extends HttpServlet {
 	@Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
         System.out.println("Salida GET modificada con webhook 333");
-        response.getWriter().println("Salida GET version master webhook 888");  
+        response.getWriter().println("Salida GET version master webhook 333");  
         response.getWriter().flush();
     }
 
